cf6eff359c13b50cde21494c93672a78d3cdbd1c
Christopher Jackson <cjacksoncmyk@gmail.com>
2023-08-13 17:29:58 -0400

    format output string exercise

---
+++ scrap_3(format output string by positions).py
@@ -0,0 +1,6 @@
+firstName = input('Enter first name ')
+lastName = input('Enter last name ')
+organization = input('Enter organization name ')
+
+print('\n')
+print('{0}, {1} works at {2}'.format(firstName, lastName, organization))
